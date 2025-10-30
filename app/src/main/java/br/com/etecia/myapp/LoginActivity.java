7475dcfab99b6de6fefa9af8381a75e33fdfa3be
Carbecas <isabelevanni@gmail.com>
2025-10-30 20:18:33 -0300

    Atualizações

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
--- app/src/main/java/br/com/etecia/myapp/RecuperarSenhaActivity.java
+++ app/src/main/java/br/com/etecia/myapp/RecuperarSenhaActivity.java
@@ -1,7 +1,6 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.button.MaterialButton;
-
-public class CadastrarActivity extends AppCompatActivity {
-
-    MaterialButton btnEnviar;
+public class RecuperarSenhaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.cadastrar_layout);
+        setContentView(R.layout.recuperar_senha_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btnEnviar = findViewById(R.id.btnEnviar);
     }
 }
