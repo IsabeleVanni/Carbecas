348615c84d061c20ddf8ff36f841ca1814467fb1
IsabeleVanni <isabelevanni@gmail.com>
2026-02-27 21:44:50 -0300

    tudo lincado caraio

--- app/src/main/java/br/com/etecia/myapp/NovasenhaActivity.java
+++ app/src/main/java/br/com/etecia/myapp/NovasenhaActivity.java
@@ -1,8 +1,6 @@
 package br.com.etecia.myapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputEditText;
-
-public class RecuperarSenhaActivity extends AppCompatActivity {
-
-    MaterialButton btnEnviar;
-    TextInputEditText txtRecupera;
+public class NovasenhaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.recuperar_senha_layout);
+        setContentView(R.layout.novasenha_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        btnEnviar = findViewById(R.id.btnEnviar);
-
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), CodigoSenhaActivity.class));
-                finish();
-            }
         });
     }
 }
